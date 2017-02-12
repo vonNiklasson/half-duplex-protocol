4abe835952974e7730f5eb1b18002f2a04a2e340
Johan Niklasson <johan@nicklassons.se>
2017-02-12 23:51:01 +0100

    Testing floating error.

--- src/protocol.c
+++ src/protocol.c
@@ -120,13 +120,17 @@ unsigned char hdp_recieve(void) {
         bitrate_previous_bit = !bitrate_previous_bit;
     }
 
+    if (DEBUG) { platform_debug("Getting here 1", 0); }
     /* Calculate the avarage delay per bit */
     _recieve_delay_per_bit = _recieve_delay_per_bit / (BITRATE_BITS_RESERVED);
 
+    if (DEBUG) { platform_debug("Getting here 2", 0); }
     /* Purposfully divide by 100 to force a rounding of the number */
-    _recieve_delay_per_bit = (100/_recieve_delay_per_bit);
-    _recieve_delay_per_bit = 100 / _recieve_delay_per_bit;
+    _recieve_delay_per_bit = (100.0/_recieve_delay_per_bit);
+    if (DEBUG) { platform_debug("Getting here 3", 0); }
+    _recieve_delay_per_bit = 100.0 / _recieve_delay_per_bit;
 
+    if (DEBUG) { platform_debug("Getting here 4", 0); }
     if (DEBUG) { platform_debug("Delay per bit", _recieve_delay_per_bit); }
     if (DEBUG) { platform_debug("Bitrate", (1000/_recieve_delay_per_bit)); }
 
